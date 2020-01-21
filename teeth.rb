93c650f2f6c4fa4181f748f8225343562ba4881e
Martha D <45017656+mdempkey@users.noreply.github.com>
2020-01-21 11:21:19 -0800

    Add files via upload

---
+++ teeth.rb
@@ -0,0 +1,151 @@
+use_bpm 140
+
+2.times do
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :c3
+  sleep 0.5
+  play :bb2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :bb2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+live_loop :teeth do
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+#measure 1
+sample"C:/Users/martha_dempkey/Downloads/luke_teeth.wav"
+sleep 0.35
+play :e4
+sleep 0.5
+play :e4
+sleep 0.5
+play :e4
+sleep 0.5
+play :f4
+sleep 0.5
+
+sleep 0.2
+play :g4
+sleep 0.5
+play :f4
+sleep 0.5
+play :e4
+sleep 0.5
+play :d4
+sleep 0.5
+
+
+play :e4
+sleep 1
+sleep 3
+
+
+play :e4
+sleep 0.5
+play :e4
+sleep 0.5
+play :e4
+sleep 0.5
+play :f4
+sleep 0.5
+
+play :g4
+sleep 0.5
+play :f4
+sleep 0.5
+play :e4
+sleep 0.5
+play :d4
+sleep 0.5
+
+
+play :e4
+sleep 1
+play :g4
+sleep 1
+play :e4
+sleep 0.5
+play :d4
+sleep 1
+
+play :f4
+sleep 1
+play:g4
+sleep 0.5
+play :a4
+sleep 1
+play :e4
+sleep 1
+play :a4
+sleep 1
+play:g4
+sleep 0.5
+play:a4
+sleep 1
+play :e4
+sleep 2.5
+
+
+play :f4
+sleep 1
+play:g4
+sleep 0.5
+play:a4
+sleep 1
+play :e4
+sleep 1
+play :a4
+sleep 1
+play:g4
+sleep 0.5
+play :a4
+sleep 1
+play :e4
+sleep 1
+play :g4
+sleep 0.5
+play:f4
+sleep 1
+
